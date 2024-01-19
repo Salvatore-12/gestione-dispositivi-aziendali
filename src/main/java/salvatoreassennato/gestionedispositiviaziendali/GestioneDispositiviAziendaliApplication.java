2bbba38a09544923c9e8c8e911d21336ffb87129
Salvatore Assennato <137400679+Salvatore-12@users.noreply.github.com>
2024-01-19 10:23:51 +0100

    start-project

---
+++ src/main/java/salvatoreassennato/gestionedispositiviaziendali/GestioneDispositiviAziendaliApplication.java
@@ -0,0 +1,13 @@
+package salvatoreassennato.gestionedispositiviaziendali;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestioneDispositiviAziendaliApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestioneDispositiviAziendaliApplication.class, args);
+	}
+
+}
---
+++ src/test/java/salvatoreassennato/gestionedispositiviaziendali/GestioneDispositiviAziendaliApplicationTests.java
@@ -0,0 +1,13 @@
+package salvatoreassennato.gestionedispositiviaziendali;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestioneDispositiviAziendaliApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
